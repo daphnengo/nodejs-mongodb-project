09f603eded2046bf0bd0657887d32617a9694cae
Daphne Ngo <dn.ngo29@gmail.com>
2024-08-08 00:53:13 -0400

    getProductDetails

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const { getDb } = require('../util/database');
 
 class Product {
@@ -10,6 +11,7 @@ class Product {
 
   save() {
     const db = getDb();
+
     // In MongoDB, we have Database, Collection and Document
     // "products" is a collection
     return db.collection('products')
@@ -29,6 +31,26 @@ class Product {
       .then(products => {
         console.log(products);
         return products;
+      })
+      .catch(error => console.log(error));
+  }
+
+  static findProductById(productId) {
+    const db = getDb();
+
+    return db.collection('products')
+      .find({ _id: mongodb.ObjectId.createFromHexString(productId) })
+      .next()
+      .then(product => {
+        // {
+        //   _id: new ObjectId('66b195c62603f85f666c3e8b'),
+        //   title: 'Doraemon',
+        //   price: '14.99',
+        //   description: 'Gadget Cat From The Future',
+        //   imageUrl: 'https://i.pinimg.com/564x/54/7a/7f/547a7f9693b6ab79efcd963d2d760fcf.jpg\n'
+        // }
+        console.log(product);
+        return product;
       })
       .catch(error => console.log(error));
   }
